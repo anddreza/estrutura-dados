d7981d2880dc78e66f07398a1f850fe5b07bb7a5
Andreza Silva <77905947+anddreza@users.noreply.github.com>
2023-08-14 20:32:53 -0300

    feat: implementação da atividade3, necessário testar, será alterado

--- atividade3.c
+++ atividade3.c
@@ -1,4 +1,5 @@
-/*Construa um programa que define uma estrutura de peça, que poderá ser usado para outros programas, deverá conter atributos como: código, nome, cor, peso, preço. Também uma função entrada de dados e uma função de saídas de dados. */
+/*Construa um programa que define uma estrutura de peça, que poderá ser usado para outros programas, deverá conter atributos como: código, nome, cor, peso, preço. 
+Também uma função entrada de dados e uma função de saídas de dados. */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,3 +13,54 @@ typedef struct{
 
 Peca insercao(void);
 void saida(Peca a[]);
+
+int main(void){
+	int i;
+	Peca bd[TAM];
+	
+	for(i=0;i<TAM;i++){
+		printf("---%i---\n",i);
+		bd[i]=insercao();
+	}
+	
+	impressao(bd);
+	
+	return 0;
+}
+
+Peca insercao(void){
+	Peca a;
+
+	printf("Digite o codigo: ");
+	fgets(a.codigo, stdin);
+	fflush(stdin);
+	getchar();
+	
+	printf("Digite o nome: ");
+	fgets(a.codigo, 50, stdin);
+	fflush(stdin);
+	getchar();
+
+	printf("Digite o peso: ");
+	scanf("%f",&a.peso);
+	fflush(stdin);
+	getchar();
+
+	printf("Digite o preço: ");
+	scanf("%f",&a.preco);
+	fflush(stdin);
+	getchar();
+
+	return a;
+}
+
+void impressao(Peca a[]){
+	int i;
+	
+	printf("CODIGO\tNOME\tPESO\tPREÇO\n");
+	for(i=0;i<TAM;i++){
+		printf("%.2f\t%s\t%.1f\t%.2f\n", a[i].codigo,a[i].nome,a[i].peso, a[i].preco);
+	}
+}
+
+
