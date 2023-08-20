97a05e2d5ed889beb54d650faa992806ca59aa13
Andreza <dedezasilva@live.com>
2023-08-20 19:40:26 -0300

    feat: alteração da atividade para melhor funcionamento, alterando a logica, não utilizando o struct

--- atividade5.c
+++ atividade5.c
@@ -1,26 +1,24 @@
 /*Construa um programa para calcular a área e de um círculo, a área deve ser do tipo de ponteiro. */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-typedef struct circulo{
-	float a, r, PI = 3,1415;
-}Circulo;
+void areaCirculo(float r, float *a);
 
-void areaCirculo(Circulo *eq){
+int main(void) {
+	float area, raio;
+
+	printf("Digite o raio: ");
+	scanf("%f", &raio);	
 	
-	if (delta >= 0) {
-		float delta = (eq->r) * ((eq->r) * (eq->r));
-	} else {
-		printf("\nValores negativos\n");
-	}
+	areaCirculo(raio, &area);
+
+	printf("A area e %f\n", area);
+	
+	return 0; 
 }
 
-int main() {
-	Circulo eq;
-	printf("Digite A: ");
-	scanf("%f", &eq.a);
-	fflush(stdin);
-	
-	return 0;
+void areaCirculo(float r, float *a){
+	*a = M_PI * (r * r);
 }
